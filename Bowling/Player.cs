1af3bc44394a29804157d5deea79852fbd421be3
sickshit <sickshit>
2020-11-30 22:41:06 +0100

    first commit

---
+++ Bowling/Player.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Bowling
+{
+    public class Player
+    {
+        public Player()
+        {
+        }
+    }
+}
---
+++ bowling_game/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace bowling_game
+{
+    public class Class1
+    {
+    }
+}
